346b7952c51224ec1263a2dfa8b6440d67f0f89f
David Chong <djchong24@gmail.com>
2020-10-25 18:08:02 -0400

    Finished isColMagic with 2 tests

--- Tester.java
+++ Tester.java
@@ -6,6 +6,7 @@ public class Tester{
         int [] arr2 = {-1, -3, -5, -9};
         int [][] arr2D =new int[][]{{1,3,5,9},{2,4,6,8}};
         int [][] goodRow = new int[][]{{1, 2, 3, 4}, {3, 2 ,4, 1}, {3, 4, 1 , 2}};
+        int [][] goodCol = new int[][]{{1,1,1},{2,2,2},{3,3,3}};
         System.out.println(ArrayOps.sum(arr1));
         System.out.println(ArrayOps.sum(arr2));
         System.out.println(ArrayOps.largest(arr1));
@@ -16,5 +17,7 @@ public class Tester{
         System.out.println(Arrays.toString(ArrayOps.sumCols(arr2D)));
         System.out.println(ArrayOps.isRowMagic(goodRow));
         System.out.println(ArrayOps.isRowMagic(arr2D));
+        System.out.println(ArrayOps.isColMagic(goodRow));
+        System.out.println(ArrayOps.isColMagic(goodCol));
     }
 }

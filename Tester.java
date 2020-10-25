5ab648ac86ec6165df36c920af92ececeb2d9cc9
David Chong <djchong24@gmail.com>
2020-10-24 20:16:16 -0400

    whoops just looked at rocket chat didnt realize we needed to push the test file as well

---
+++ Tester.java
@@ -0,0 +1,12 @@
+public class Tester{
+    public static void main(String[] args) {
+        int [] arr1 = {1, 3, 5, 9};
+        int [] arr2 = {-1, -3, -5, -9};
+        int [][] arr2D =new int[][]{{1,3,5,9},{2,4,6,8}};
+        System.out.println(ArrayOps.sum(arr1));
+        System.out.println(ArrayOps.sum(arr2));
+        System.out.println(ArrayOps.largest(arr1));
+        System.out.println(ArrayOps.largest(arr2));
+        System.out.println(ArrayOps.sumRows(arr2D));
+    }
+}
